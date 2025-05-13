699d66cee272c8729994f887ad223433bbacf93f
Derick Felix <dericksanfel@gmail.com>
2025-05-13 16:30:31 -0300

    Exercícios php 13/05

--- exercicios php/4.php
+++ exercicios php/4.php
@@ -1,10 +1,12 @@
 <h1 style="text-align: center;">Calculador de parcelas com juros</h1>
 <form action="4.php" method="post">
-    Valor do produto:  R$<input type="number" name="valor" /><br><br>
+    Valor do produto:  R$<input type="number" name="valor" /><br>
+    Juros:  <input type="number" name="juros" />%<br>
+    Número de parcelas:  <input type="number" name="numParc" /><br><br>
     <input type="submit" name="submit" value="Enviar" />
 </form>
 <?php
-$valorTotal = $_REQUEST['valor'] + $_REQUEST['valor'] * 0.16;
-$valorParcela = $valorTotal / 10;
+$valorTotal = $_REQUEST['valor'] + $_REQUEST['valor'] * ($_REQUEST['juros'] / 100);
+$valorParcela = $valorTotal / $_REQUEST['numParc'];
 echo "<br>Valor da parcela: R$" . $valorParcela . "<br>Valor total: R$" . $valorTotal;
 ?>
